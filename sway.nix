0350eadec8715056c7b19e619995b8d2ed569cf4
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2023-11-27 00:48:55 -0500

    waybar

--- sway.nix
+++ sway.nix
@@ -8,6 +8,7 @@
             terminal = "alacritty";
             window = {border = 2; titlebar = false;};
             bars = [];
+            output."*".bg = "~/wallpaper.png fill";
 
             keybindings = lib.mkOptionDefault {
                 "${modifier}+Shift+f" = "exec firefox";
@@ -21,7 +22,25 @@
                 "${modifier}+Shift+l" = "move right";
                 "${modifier}+b" = "split h";
                 "${modifier}+v" = "split v";
+                "${modifier}+1" = "workspace number 1";
+                "${modifier}+2" = "workspace number 2";
+                "${modifier}+3" = "workspace number 3";
+                "${modifier}+4" = "workspace number 4";
+                "${modifier}+5" = "workspace number 5";
+                "${modifier}+6" = "workspace number 6";
+                "${modifier}+7" = "workspace number 7";
+                "${modifier}+8" = "workspace number 8";
+                "${modifier}+9" = "workspace number 9";
                 "${modifier}+0" = "workspace number 10";
+                "${modifier}+Shift+1" = "move container to workspace number 1";
+                "${modifier}+Shift+2" = "move container to workspace number 2";
+                "${modifier}+Shift+3" = "move container to workspace number 3";
+                "${modifier}+Shift+4" = "move container to workspace number 4";
+                "${modifier}+Shift+5" = "move container to workspace number 5";
+                "${modifier}+Shift+6" = "move container to workspace number 6";
+                "${modifier}+Shift+7" = "move container to workspace number 7";
+                "${modifier}+Shift+8" = "move container to workspace number 8";
+                "${modifier}+Shift+9" = "move container to workspace number 9";
                 "${modifier}+Shift+0" = "move container to workspace number 10";
                 "${modifier}+Shift+r" = "reload";
                 "${modifier}+u" = "resize shrink width 3 px or 3 ppt";
@@ -89,6 +108,7 @@
                 repeat_delay 200
                     repeat_rate 50
             }
+            workspace 1
         '';
     };
 
--- waybar.nix
+++ waybar.nix
@@ -1,5 +1,50 @@
-#{pkgs, lib, ...}: {
-    #programs.waybar = {
-        #enable = true;
-    #};
-#}
+{pkgs, lib, ...}: {
+    programs.waybar.enable = true;
+    programs.waybar.settings = {
+        mainBar = {
+            layer = "top";
+            position = "bottom";
+            height = 40;
+            modules-left = [ "sway/workspaces" ];
+            modules-center = [ ];
+            modules-right = [ "memory" "cpu" "temperature" "network" "pulseaudio" "clock" ];
+
+            memory = {
+                interval = 10;
+                format = "Mem {percentage}%";
+            };
+
+            cpu = {
+                interval = 5;
+                format = "CPU {usage}% {icon0}{icon1}{icon2}{icon3}{icon4}{icon5}{icon6}{icon7}";
+                format-icons = ["▁" "▂" "▃" "▄" "▅" "▆" "▇" "█"];
+            };
+
+            temperature = {
+                format = "{temperatureC}C";
+            };
+
+            network = {
+                format = "Net {signalStrength}";
+            };
+
+            pulseaudio = {
+                format = "Vol {volume}";
+            };
+
+            clock = {
+                format = "{:%H:%M %a %d %h %Y}";
+            };
+
+        };
+    };
+    programs.waybar.style = ''
+        * {
+            border: none;
+            border-radius: 0;
+            font-family: "Source Code Pro";
+            font-size: 16px;
+            min-height: 0;
+        }
+    '';
+}
