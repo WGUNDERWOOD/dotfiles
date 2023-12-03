28c3ec2b48bbd39027564879cf3061632868c111
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2023-12-03 18:49:25 -0500

    rclone ignore

--- rclone/rclone_google_drive_princeton_push.sh
+++ rclone/rclone_google_drive_princeton_push.sh
@@ -8,7 +8,7 @@ FAR="google_drive_princeton:"
 echo -e "\e[0;35m\033[1mrclone push Google Drive Princeton\e[0;30m\033[0m"
 
 echo -e "\e[0;33m\033[1mmy files\e[0;30m\033[0m"
-rclone sync -i -u --exclude-from "$EXCLUDE" "$NEAR" "$FAR"
+rclone sync -i -u --checkers 8 --exclude-from "$EXCLUDE" "$NEAR" "$FAR"
 
 echo -e "\e[0;33m\033[1mshared files\e[0;30m\033[0m"
-rclone sync -i -u --drive-shared-with-me --exclude-from "$EXCLUDE" "$NEARSHARED" "$FAR"
+rclone sync -i -u --checkers 8 --drive-shared-with-me --exclude-from "$EXCLUDE" "$NEARSHARED" "$FAR"
