09cb6d5eb558fb65dcf5f148d6fb26ce73121ba8
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2023-12-06 18:24:16 -0500

    latex scripts

--- latex/latex.nix
+++ latex/latex.nix
@@ -2,6 +2,9 @@
     home.file.".latexmkrc".source = ./.latexmkrc;
     home.file."scripts/texclean".source = ./texclean;
     home.file."scripts/tex_log_parse".source = ./tex_log_parse;
+    home.file."scripts/bibtex-download".source = ./bibtex-download;
+    home.file."scripts/latexindent_fast".source = ./bibtex-download;
+    home.file."scripts/tex_labels".source = ./bibtex-download;
     home.file.".indentsettings.yaml".source = ./.indentsettings.yaml;
     home.file.".indentconfig.yaml".text = ''
         paths:
