6e526a62f7a971c61f38e5a28f2395f388bb5289
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2023-12-26 01:37:29 +0000

    simplify tex check

--- tex-check/src/main.rs
+++ tex-check/src/main.rs
@@ -1,14 +1,9 @@
+use regex::Regex;
 use std::env;
 use std::fs;
-use regex::Regex;
 
 // declare matches
-const MATCH: [&str; 4] = [
-    "error",
-    "warn",
-    "overfull",
-    "underfull",
-];
+const MATCH: [&str; 4] = ["error", "warn", "overfull", "underfull"];
 
 // declare non matches
 const NOMATCH: [&str; 3] = [
@@ -24,14 +19,15 @@ const RED: &str = "\x1b[31m\x1b[1m";
 const RESET: &str = "\x1b[00m\x1b[0m";
 
 fn main() {
-
     // get filenames from arguments
     let args: Vec<String> = env::args().collect();
     assert!(args.len() >= 2, "No file provided");
     let filenames = &args[1..];
 
     // check file is correct format
-    assert!(filenames.iter().all(|f| f.ends_with(".log") || f.ends_with(".blg")));
+    assert!(filenames
+        .iter()
+        .all(|f| f.ends_with(".log") || f.ends_with(".blg")));
 
     // get regexes
     let matches = MATCH.map(|w| Regex::new(&(String::new() + "(?i)" + w)).unwrap());
@@ -73,19 +69,10 @@ fn main() {
         if warnings.len() > 0 {
             println!("{}", String::new() + YELLOW + filename + RESET);
             for w in &warnings {
-
-                //let mut formatted_warning = w.1.to_string();
-
-                // format warning words
-                //for i in 0..matches.len() {
-                    //let re = &matches[i];
-                    //let m = &MATCH[i];
-                    //let colored_m = String::new() + RED + &m.to_string() + RESET;
-                    //formatted_warning = re.replace_all(&formatted_warning, colored_m).to_string();
-                //}
-
-                print!("{}", String::new() + GREEN + &(w.0 + 1).to_string() + ": " + RESET);
-                //print!("{}", formatted_warning);
+                print!(
+                    "{}",
+                    String::new() + GREEN + &(w.0 + 1).to_string() + ": " + RESET
+                );
                 print!("{}", String::new() + RED + w.1 + RESET);
                 println!();
             }
