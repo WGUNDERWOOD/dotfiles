9c7835d63f582b4e095f8e5ddabe9fbcc58d8dd3
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2024-01-07 17:51:09 -0500

    pdf-insert-blank-pages

---
+++ programs/pdf-insert-blank-pages.nix
@@ -0,0 +1,42 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.writeShellApplication {
+    name = "pdf-insert-blank-pages";
+    runtimeInputs = with pkgs; [ coreutils pdftk texlive.combined.scheme-full ];
+    text = ''
+        # get directory names
+        current_dir="$(pwd)"
+        new_dir="$(mktemp -d)"
+
+        # copy to new directory
+        mkdir -p "$new_dir"
+        cp "$1" "$new_dir/in.pdf"
+        cd "$new_dir"
+
+        # make blank page
+        printf "
+            \\\documentclass{article}
+            \\\pagenumbering{gobble}
+            \\\begin{document}
+            \\\mbox{}
+            \\\end{document}
+            " > blank.tex
+        latexmk -pdf -quiet -rc-report- blank
+
+        # insert blank pages
+        pdftk in.pdf burst
+        for i in pg*.pdf; do
+            printf "%s\nblank.pdf\n" "$i" >> list;
+        done
+        args=()
+        mapfile -t args < "list"
+        pdftk "''${args[@]}" cat output out.pdf
+
+        # move back
+        cd "$current_dir"
+        filename="$(basename "$1" ".pdf")"
+        cp "$new_dir/out.pdf" "$current_dir/''${filename}_blanks.pdf"
+        rm -r "$new_dir"
+        '';
+}
+
