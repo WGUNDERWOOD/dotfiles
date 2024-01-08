02e3d476781a93c512e00bbb085a807a63fe3b40
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2024-01-08 00:56:32 -0500

    compress pdf multiple runs

--- programs/compress-pdf/compress-pdf.sh
+++ programs/compress-pdf/compress-pdf.sh
@@ -1,7 +1,5 @@
 #!usr/bin/env bash
 
-# TODO allow multiple runs
-
 # colors
 RED='\033[1;31m'
 YELLOW='\033[1;33m'
@@ -39,7 +37,7 @@ tempdir=$(mktemp -d)
 optimize_perfect() {
     gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dNOPAUSE -dQUIET -dBATCH \
         -dDetectDuplicateImages -dCompressFonts=true \
-        -sOutputFile="${tempfile}" "${infile}"
+        -sOutputFile="${bfile}" "${afile}"
 }
 
 # high quality optimization
@@ -47,7 +45,7 @@ optimize_high() {
     gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dNOPAUSE -dQUIET -dBATCH \
         -dDetectDuplicateImages -dCompressFonts=true \
         -dPDFSETTINGS=/ebook \
-        -sOutputFile="${tempfile}" "${infile}"
+        -sOutputFile="${bfile}" "${afile}"
 }
 
 # low quality optimization
@@ -55,7 +53,7 @@ optimize_low() {
     gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dNOPAUSE -dQUIET -dBATCH \
         -dDetectDuplicateImages -dCompressFonts=true \
         -dPDFSETTINGS=/screen \
-        -sOutputFile="${tempfile}" "${infile}"
+        -sOutputFile="${bfile}" "${afile}"
 }
 
 # error if not all files are pdfs
@@ -68,37 +66,79 @@ done
 
 for infile in "$@"; do
 
-    # get initial file size
+    # set up variables and get initial file size
     infilebase="$(basename "$infile" .pdf)"
-    tempfile="${tempdir}/${infilebase}_cmp.pdf"
+    cmp1file="${tempdir}/${infilebase}_cmp1.pdf"
+    cmp2file="${tempdir}/${infilebase}_cmp2.pdf"
     outfile="$infilebase".pdf
-    insize=$(stat -c "%s" "${infile}")
-    insizeh=$(du -h "${infile}" | cut -f -1)
+    insize=$(du -b "${infile}" | cut -f -1)
+    insizeh=$(du -bh "${infile}" | cut -f -1)
     printf "%b%-40.40s  %b%s %b-> %b" \
         "$PURPLE" "$infile" "$PINK" "$insizeh" "$WHITE" "$RESET"
 
     # run the selected optimizer
+    afile="$infile"
+    bfile="$cmp1file"
     case $quality in
         perfect) optimize_perfect ;;
         high) optimize_high ;;
         low) optimize_low ;;
     esac
 
-    # get new size
-    tempsize=$(stat -c "%s" "${tempfile}")
-    tempsizeh=$(du -h "${tempfile}" | cut -f -1)
+    # get first compression size
+    cmp1size=$(du -b "${cmp1file}" | cut -f -1)
+    cmp1sizeh=$(du -bh "${cmp1file}" | cut -f -1)
 
-    # handle new size
-    if [ "$tempsize" -eq 0 ]; then
-        echo "No output, keeping original"
+    # first compression size is zero
+    if [ "$cmp1size" -eq 0 ]; then
         printf "%bNo output, keeping original%b\n" "$RED" "$RESET"
-    fi
-    if [ "$tempsize" -ge "$insize" ]; then
-        printf "%b%s%b\n" "$RED" "$tempsizeh" "$RESET"
+
+    # first compression size is larger
+    elif [ "$cmp1size" -ge "$insize" ]; then
+        printf "%b%s%b\n" "$RED" "$cmp1sizeh" "$RESET"
+
+    # first compression size is much smaller
+    elif [ "$cmp1size" -le "$(( 9 * "$insize" / 10 ))" ]; then
+
+        cmp1percent=$(("$cmp1size" * 100 / "$insize"))
+        printf "%b%s (%s%%)%b" "$GREEN" "$cmp1sizeh" "$cmp1percent" "$RESET"
+        printf "%b -> %b" "$WHITE" "$RESET"
+
+        # compress again
+        afile="$cmp1file"
+        bfile="$cmp2file"
+        case $quality in
+            perfect) optimize_perfect ;;
+            high) optimize_high ;;
+            low) optimize_low ;;
+        esac
+
+        # get second compression size
+        cmp2size=$(du -b "${cmp2file}" | cut -f -1)
+        cmp2sizeh=$(du -bh "${cmp2file}" | cut -f -1)
+
+        # second compression size is zero
+        if [ "$cmp2size" -eq 0 ]; then
+            printf "%bNo output, keeping first compression%b\n" "$RED" "$RESET"
+            cp "$cmp1file" "$outfile"
+
+        # second compression size is larger
+        elif [ "$cmp2size" -ge "$cmp1size" ]; then
+            printf "%b%s%b\n" "$RED" "$cmp2sizeh" "$RESET"
+            cp "$cmp1file" "$outfile"
+
+        # second compression size is smaller
+        else
+            cmp2percent=$(("$cmp2size" * 100 / "$insize"))
+            printf "%b%s (%s%%)%b\n" "$GREEN" "$cmp2sizeh" "$cmp2percent" "$RESET"
+            cp "$cmp2file" "$outfile"
+        fi
+
+    # first compression size is a little smaller
     else
-        percentage=$(("$tempsize" * 100 / "$insize"))
-        printf "%b%s (%s%%)%b\n" "$GREEN" "$tempsizeh" "$percentage" "$RESET"
-        cp "$tempfile" "$outfile"
+        cmp1percent=$(("$cmp1size" * 100 / "$insize"))
+        printf "%b%s (%s%%)%b\n" "$GREEN" "$cmp1sizeh" "$cmp1percent" "$RESET"
+        cp "$cmp1file" "$outfile"
     fi
 
 done
