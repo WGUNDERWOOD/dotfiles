4ea0fa6fa30c76e2cb1a833ef7a5272fa713a9c2
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2024-02-25 15:16:45 -0500

    spell check deps

--- programs/spell-check/spell-check.nix
+++ programs/spell-check/spell-check.nix
@@ -2,6 +2,11 @@
 pkgs.rustPlatform.buildRustPackage rec {
   pname = "spell-check";
   version = "0.1.0";
+  propagatedBuildInputs = with pkgs; [
+      coreutils
+      aspell
+      aspellDicts.en
+  ];
   cargoLock.lockFile = ./Cargo.lock;
   src = pkgs.lib.cleanSource ./.;
 }
