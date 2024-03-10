c91b6924a8e68b13a2f7d6994ff69eb639779c2a
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2024-03-10 14:23:42 -0400

    ignore

--- programs/tex-build/tex-build.sh
+++ programs/tex-build/tex-build.sh
@@ -1,60 +1,62 @@
 #!/usr/bin/env bash
 
-# TODO -l for -pdflua
-# TODO -x for -xelatex
-# TODO -e for -shell-escape
-# TODO -n for -norc
+# TODO only copy pdf when it is ready, use latexmk -outdir
+# TODO print options if work being done
+# TODO print file name
 
-# TODO -quiet standard
-# TODO -rc-report- standard
-# TODO -Werror standard
-# TODO grep for ignore nothing to do standard
+# colors
+RED='\033[1;31m'
+YELLOW='\033[1;33m'
+PURPLE='\033[1;34m'
+PINK='\033[1;35m'
+GREEN='\033[1;32m'
+WHITE='\033[1;37m'
+RESET='\033[0m'
 
-# TODO -v to omit -quiet (verbose)
-# TODO -r to omit -rc-report- (include rc report)
-# TODO -w to omit -Werror (warnings are not errors)
+# get flags from user
+pdf="-pdf"
+quiet="-quiet"
+report="-rc-report-"
+warn="-Werror"
 
-# TODO handle files in another directory
-# TODO handle multiple files
-# TODO only copy pdf when ready
+while getopts 'lxenvrw' flag; do
+    case "${flag}" in
+        l) pdf="-pdflua" ;;
+        x) pdf="-xelatex" ;;
+        e) esc="-shell-escape" ;;
+        n) norc="-norc" ;;
+        v) quiet="" ;;
+        r) report="" ;;
+        w) warn="" ;;
+        *) exit 1 ;;
+    esac
+done
 
-# get quality level from flags
-#quality='perfect'
-#while getopts 'hl' flag; do
-    #case "${flag}" in
-        #h) quality='high' ;;
-        #l) quality='low' ;;
-        #*) exit 1 ;;
-    #esac
-#done
+shift $((OPTIND - 1))
 
 # error if no files provided
-#if [[ -z "$*" ]]; then
-    #printf "%bno input file(s) specified%b\n" "$RED" "$RESET"
-    #exit 1
-#fi
+if [[ -z "$*" ]]; then
+    printf "%bno input file(s) specified%b\n" "$RED" "$RESET"
+    exit 1
+fi
 
-# error if not all files are pdfs
-#for infile in "$@"; do
-    #if [ "${infile: -4}" != ".pdf" ]; then
-        #printf "%b%s %bis not a pdf%b\n" "$PURPLE" "$infile" "$RED" "$RESET"
-        #exit 1
-    #fi
-#done
+# error if not all files are tex
+for file in "$@"; do
+    if [ "${file: -4}" != ".tex" ]; then
+        printf "%b%s %bis not a tex file%b\n" "$PURPLE" "$file" "$RED" "$RESET"
+        exit 1
+    fi
+done
 
-#for infile in "$@"; do
+for file in "$@"; do
 
-    # set up variables and get initial file size
-    #infilebase="$(basename "$infile" .pdf)"
-    #outdir="$(dirname "$infile")"
-    #shafile="${outdir}/${infilebase}.pdf.sha256"
-    #cmpfile="${tempdir}/${infilebase}_cmp.pdf"
-    #outfile="$infilebase".pdf
-    #insize=$(du -b "${infile}" | cut -f -1)
-    #insizeh=$(du -bh "${infile}" | cut -f -1)
-    #printf "%b%-40.40s  %b%-4.4s %b-> %b" \
-        #"$PURPLE" "$infile" "$PINK" "$insizeh" "$WHITE" "$RESET"
-#done
+    filepath="$(realpath "$file")"
+    dir="$(dirname "$filepath")"
+    (cd $dir &&
+        latexmk "$pdf" "$quiet" "$report" "$warn" "$filepath" | \
+            grep -v 'Latexmk: Nothing to do for' | grep '^.' || true
+    )
 
-# clean up
-#exit 0
+done
+
+exit 0
