54da82d87cbe155fdf12a36b7ca9ffc16f131453
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2024-03-09 20:15:18 -0500

    latexfmt

--- programs/latexfmt/texfmt.py
+++ programs/latexfmt/texfmt.py
@@ -11,32 +11,47 @@ def remove_comment(line):
     return re.sub("%.*", "", line)
 
 def get_back(line):
+    # no deindentation for ending document
     if re.match(r".*\\end{document}.*", line): return 0
+    # list environments get double indents for indenting items
+    for l in LISTS:
+        if re.match(r".*\\end{%s}.*" % l, line): return 2
+    # other environments get single indents
     if re.match(r".*\\end{[a-z\*]*}.*", line): return 1
+    # deindent items to make the rest of item environment appear indented
+    if re.match(r".*\\item.*", line): return 1
     back = 0
     cumul = 0
     for c in line:
-        cumul -= (c in ["(", "{", "["])
-        cumul += (c in [")", "}", "]"])
+        cumul -= (c in OPENS)
+        cumul += (c in CLOSES)
         back = max(cumul, back)
     return back
 
 def get_diff(line):
+    # no indentation for document
     if re.match(r".*\\begin{document}.*", line): return 0
     if re.match(r".*\\end{document}.*", line): return 0
-    if re.match(r".*\\begin{[a-z\*]*}.*", line): return 1
-    if re.match(r".*\\end{[a-z\*]*}.*", line): return -1
     diff = 0
-    diff += line.count("(")
-    diff -= line.count(")")
-    diff += line.count("{")
-    diff -= line.count("}")
-    diff += line.count("[")
-    diff -= line.count("]")
+    # list environments get double indents
+    for l in LISTS:
+        if re.match(r".*\\begin{%s}.*" % l, line): diff += 1
+        if re.match(r".*\\end{%s}.*" % l, line): diff -= 1
+    # other environments get single indents
+    if re.match(r".*\\begin{[a-z\*]*}.*", line): diff += 1
+    if re.match(r".*\\end{[a-z\*]*}.*", line): diff -= 1
+    # delimiters
+    for c in OPENS:
+        diff += line.count(c)
+    for c in CLOSES:
+        diff -= line.count(c)
     return diff
 
-# set tab length
+# set constants
 TAB = 2
+OPENS = ["(", "[", "{", "``"]
+CLOSES = [")", "]", "}", "''"]
+LISTS = ["itemize", "enumerate", "description"]
 
 # open file
 filename = sys.argv[1]
@@ -74,3 +89,7 @@ for i in range(len(lines)):
         new_line = (indents[i] * TAB * " ") + new_line
     new_lines[i] = new_line
     print(new_line)
+
+# backup original file
+# TODO do this with absolute paths
+# TODO handle files in other dirs
