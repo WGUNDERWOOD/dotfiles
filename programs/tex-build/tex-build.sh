826bf358a3789c2dca3ed629af70198a130d5a35
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2024-03-10 17:47:57 -0400

    tex build

--- programs/tex-build/tex-build.sh
+++ programs/tex-build/tex-build.sh
@@ -1,38 +1,39 @@
 #!/usr/bin/env bash
 
-# TODO only copy pdf when it is ready, use latexmk -outdir
-# TODO print options if work being done
-# TODO print file name
+set -o errexit
+set -o pipefail
 
 # colors
 RED='\033[1;31m'
-YELLOW='\033[1;33m'
 PURPLE='\033[1;34m'
-PINK='\033[1;35m'
-GREEN='\033[1;32m'
-WHITE='\033[1;37m'
 RESET='\033[0m'
 
 # get flags from user
-pdf="-pdf"
 quiet="-quiet"
 report="-rc-report-"
 warn="-Werror"
 
-while getopts 'lxenvrw' flag; do
+while getopts 'lxenvw' flag; do
     case "${flag}" in
         l) pdf="-pdflua" ;;
         x) pdf="-xelatex" ;;
         e) esc="-shell-escape" ;;
         n) norc="-norc" ;;
-        v) quiet="" ;;
-        r) report="" ;;
+        v) quiet=""; report="" ;;
         w) warn="" ;;
         *) exit 1 ;;
     esac
 done
 
 shift $((OPTIND - 1))
+
+# get around empty flags
+if [[ -z "$pdf" ]]; then pdf="-pdf"; fi
+if [[ -z "$quiet" ]]; then quiet="$pdf"; fi
+if [[ -z "$report" ]]; then report="$pdf"; fi
+if [[ -z "$warn" ]]; then warn="$pdf"; fi
+if [[ -z "$esc" ]]; then esc="$pdf"; fi
+if [[ -z "$norc" ]]; then norc="$pdf"; fi
 
 # error if no files provided
 if [[ -z "$*" ]]; then
@@ -50,11 +51,17 @@ done
 
 for file in "$@"; do
 
+    # get absolute paths and print file
     filepath="$(realpath "$file")"
     dir="$(dirname "$filepath")"
-    (cd $dir &&
-        latexmk "$pdf" "$quiet" "$report" "$warn" "$filepath" | \
-            grep -v 'Latexmk: Nothing to do for' | grep '^.' || true
+    printf "%b%s%b\n" "$PURPLE" "$file" "$RESET"
+
+    # run latexmk
+    (
+    cd "$dir"
+    latexmk "$pdf" "$quiet" "$report" "$warn" "$esc" "$norc" "$filepath" | \
+        rg -v "Latexmk: Nothing to do for" | \
+        (rg --color never "^." || true)
     )
 
 done
