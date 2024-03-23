0661a152398f781625856f7c4509c684d14cdb73
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2024-03-23 12:41:41 -0400

    git diff

--- programs/img-diff/img-diff.sh
+++ programs/img-diff/img-diff.sh
@@ -1,8 +1,5 @@
 #!usr/bin/env bash
-
-TEMP="$HOME/tmp/temp.png"
-DIFF="$HOME/tmp/diff.png"
-
-compare "$1" "$2" "$TEMP"
-convert +append "$1" "$2" "$TEMP" "$DIFF"
-feh --scale-down "$DIFF"
+echo "$1"
+compare "$2" "$1" png:- | \
+    montage -font Source-Code-Pro -geometry +4+4 "$2" - "$1" png:- | \
+    feh --scale-down - || true
