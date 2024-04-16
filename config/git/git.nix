b785ce4bb1ff5c2b6bb44f0d59e9bda2bec0a3d8
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2024-04-16 17:10:22 -0400

    move gitattributes

--- config/git/git.nix
+++ config/git/git.nix
@@ -1,4 +1,4 @@
 {pkgs, ...}: {
   home.file.".config/git/config".source = ./.gitconfig;
-  home.file.".gitattributes".source = ./.gitattributes;
+  home.file.".config/git/.gitattributes".source = ./.gitattributes;
 }
