62c5d15195fe574888aa9fd9bc3b304059e07714
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2024-04-16 23:38:39 -0400

    revert

--- config/git/git.nix
+++ config/git/git.nix
@@ -1,4 +1,4 @@
 {pkgs, ...}: {
   home.file.".config/git/config".source = ./.gitconfig;
-  home.file.".config/git/.gitattributes".source = ./.gitattributes;
+  home.file.".gitattributes".source = ./.gitattributes;
 }
