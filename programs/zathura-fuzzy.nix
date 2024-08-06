b4cde9bf95122c77428af94f5e673368fe9ca115
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2024-08-06 17:47:55 +0100

    Rofi references

--- programs/zathura-fuzzy.nix
+++ programs/zathura-fuzzy.nix
@@ -3,7 +3,7 @@ pkgs.writeShellApplication {
   name = "zathura-fuzzy";
   runtimeInputs = with pkgs; [findutils fzf coreutils screen zathura];
   text = ''
-    file=$(find "$HOME" -type f -name '*.pdf' -print | fzf --tac)
+    file=$(find "$@" -type f -name '*.pdf' -print | fzf --tac)
     echo "$file"
     [[ -n "$file" ]] && screen -d -m zathura "$file" && sleep 0.1
   '';
