c8eff5141575301756cc7d349c3c715525a66308
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2024-08-27 17:43:29 +0100

    Fix zathura-fuzzy ignore

--- programs/zathura-fuzzy.nix
+++ programs/zathura-fuzzy.nix
@@ -3,7 +3,7 @@ pkgs.writeShellApplication {
   name = "zathura-fuzzy";
   runtimeInputs = with pkgs; [fd fzf coreutils screen zathura];
   text = ''
-    file=$(fd -t f -e pdf . "$@" | fzf --tac)
+    file=$(fd -I -t f -e pdf . "$@" | fzf --tac)
     echo "$file"
     [[ -n "$file" ]] && screen -d -m zathura "$file" && sleep 0.1
   '';
