e197e2a75fe0678b9987874dd7a30ca1b54ea6cf
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2024-09-08 17:06:27 +0100

    Update tex-fmt to v0.4.2

--- programs/tex-fmt.nix
+++ programs/tex-fmt.nix
@@ -3,7 +3,7 @@ pkgs.callPackage (
   pkgs.fetchFromGitHub {
     owner = "wgunderwood";
     repo = "tex-fmt";
-    rev = "v0.4.1";
-    sha256 = "sha256-1gHz8xHkSKu4/BkOuxRIXVfHwJPWJTh5AogETlAsuys=";
+    rev = "v0.4.2";
+    sha256 = "sha256-fm55jaw1dj5rZWz8oF1o5WtjYjxNwnrD6GGa3OgZSpA=";
   }
 ) {}
