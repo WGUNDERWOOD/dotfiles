1f9df04839bdea6a4fb4cc587c744c7ae86f07e0
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2025-05-24 17:10:35 +0100

    Spotify theming

--- config/spotify.nix
+++ config/spotify.nix
@@ -1,53 +1,63 @@
 {...}: {
   home.file.".config/spotify-player/app.toml".text = ''
     theme = "dracula"
-    client_id = "65b708073fc0480ea92a077233ca87bd"
-    client_port = 8080
-    login_redirect_uri = "http://127.0.0.1:8989/login"
     playback_format = """
-    {status} {track} • {artists}
+    {status} {track} / {artists}
     {album}
     {metadata}"""
-    notify_timeout_in_secs = 0
-    tracks_playback_limit = 50
-    app_refresh_duration_in_ms = 32
-    playback_refresh_duration_in_ms = 0
-    page_size_in_rows = 20
-    play_icon = "▶"
-    pause_icon = "▌▌"
-    liked_icon = "♥"
-    border_type = "Plain"
-    progress_bar_type = "Rectangle"
-    cover_img_length = 9
-    cover_img_width = 5
-    cover_img_scale = 1.0
-    enable_media_control = true
-    enable_streaming = "Always"
-    enable_notify = true
-    enable_cover_image_cache = true
-    default_device = "spotify-player"
-    notify_streaming_only = false
-    seek_duration_secs = 5
-
-    [notify_format]
-    summary = "{track} • {artists}"
-    body = "{album}"
-
-    [layout]
-    playback_window_position = "Top"
-    playback_window_height = 6
-
-    [layout.library]
-    playlist_percent = 40
-    album_percent = 40
+    play_icon = "PLAY"
+    pause_icon = "PAUSE"
+    liked_icon = "Y"
+    enable_notify = false
+    seek_duration_secs = 10
 
     [device]
-    name = "spotify-player"
-    device_type = "speaker"
     volume = 100
     bitrate = 320
-    audio_cache = false
+    audio_cache = true
     normalization = false
     autoplay = false
+  '';
+
+  home.file.".config/spotify-player/theme.toml".text = ''
+    [[themes]]
+    name = "dracula"
+
+    [themes.palette]
+    background = "#1e1f29"
+    foreground = "#f8f8f2"
+    black = "#000000"
+    red = "#ff5555"
+    green = "#50fa7b"
+    yellow = "#f1fa8c"
+    blue = "#bd93f9"
+    magenta = "#ff79c6"
+    cyan = "#8be9fd"
+    white = "#bbbbbb"
+    bright_black = "#555555"
+    bright_red = "#ff5555"
+    bright_green = "#50fa7b"
+    bright_yellow = "#f1fa8c"
+    bright_blue = "#bd93f9"
+    bright_magenta = "#ff79c6"
+    bright_cyan = "#8be9fd"
+    bright_white = "#ffffff"
+
+    [themes.component_style]
+    block_title = { fg = "Magenta"  }
+    border = {}
+    playback_status = { fg = "Cyan", modifiers = ["Bold"] }
+    playback_track = { fg = "Cyan", modifiers = ["Bold"] }
+    playback_artists = { fg = "Cyan", modifiers = ["Bold"] }
+    playback_album = { fg = "Yellow" }
+    playback_metadata = { fg = "BrightBlack" }
+    playback_progress_bar = { bg = "BrightBlack", fg = "Green" }
+    current_playing = { fg = "Green", modifiers = ["Bold"] }
+    page_desc = { fg = "Cyan", modifiers = ["Bold"] }
+    playlist_desc = { fg = "BrightBlack", modifiers = ["Dim"] }
+    table_header = { fg = "Blue" }
+    secondary_row = {}
+    like = {}
+    lyrics_played = { modifiers = ["Dim"] }
   '';
 }
