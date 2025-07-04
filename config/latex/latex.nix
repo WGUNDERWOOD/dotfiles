c5377e290dfca86e2836a63e5e3e889fba5c5d73
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2025-07-04 10:23:34 +0100

    Remove latexindent

--- config/latex/latex.nix
+++ config/latex/latex.nix
@@ -1,8 +1,3 @@
 {...}: {
   home.file.".config/latexmk/.latexmkrc".source = ./.latexmkrc;
-  home.file.".indentsettings.yaml".source = ./.indentsettings.yaml;
-  home.file.".indentconfig.yaml".text = ''
-    paths:
-    - /home/will/.indentsettings.yaml
-  '';
 }
