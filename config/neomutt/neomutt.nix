561846e727feefaf082b8e4b4e11e26191c351d0
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2025-07-07 10:29:29 +0100

    Mutt theme

--- config/neomutt/neomutt.nix
+++ config/neomutt/neomutt.nix
@@ -1,7 +1,7 @@
 {...}: {
   home.file.".config/neomutt/neomuttrc".source = ./neomuttrc;
   home.file.".config/neomutt/mailcap".source = ./mailcap;
-  home.file.".config/neomutt/dracula.muttrc".source = ./dracula.muttrc;
+  home.file.".config/neomutt/theme.muttrc".source = ./theme.muttrc;
   home.file.".config/neomutt/bindings.muttrc".source = ./bindings.muttrc;
   home.file.".config/neomutt/gmail.muttrc".source = ./gmail.muttrc;
   home.file.".config/neomutt/cambridge.muttrc".source = ./cambridge.muttrc;
