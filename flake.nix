02441fbacf36b33f6996316453431a012509ccfe
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2026-08-03 10:34:31 +0100

    Update to 26.05

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-25.11";
-    home-manager.url = "github:nix-community/home-manager/release-25.11";
+    nixpkgs.url = "nixpkgs/nixos-26.05";
+    home-manager.url = "github:nix-community/home-manager/release-26.05";
   };
   outputs = inputs @ {home-manager, ...}: {
     nixosConfigurations = let
